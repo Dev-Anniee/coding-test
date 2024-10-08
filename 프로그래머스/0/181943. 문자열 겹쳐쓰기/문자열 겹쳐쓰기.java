5ff6bebab628c523e956455ebf0c511773f271c6
Jeongmin An <ajm010402@gmail.com>
2024-10-08 15:28:18 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 41.64 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,12 +1,10 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
 
-        char[] answer = new char[my_string.length()];
-        answer = my_string.toCharArray();
-        for(int i =0; i<overwrite_string.length(); i++) {
-            answer[s] = overwrite_string.charAt(i);
-            s++;
-        }
-        return new String(answer);
+        String answer = "";
+        answer = my_string.substring(0,s)+overwrite_string+my_string.substring(s+overwrite_string.length());
+
+
+        return answer;
     }
 }
