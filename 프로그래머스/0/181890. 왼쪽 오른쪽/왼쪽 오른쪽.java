ac47358d2b0d38ba4495e06e1557fb8048551758
Jeongmin An <ajm010402@gmail.com>
2024-10-20 00:00:26 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,30 +1,35 @@
-import java.util.Arrays;
-
 class Solution {
     public String[] solution(String[] str_list) {
         int flag = 0;
-        int idx =0;
+        int idx = 0;
         String[] answer = {};
-        
-        for(int i=0; i<str_list.length; i++){
-            if("l".equals(str_list[i])){
-                flag=1;
-                idx=i;
+
+        for (int i = 0; i < str_list.length; i++) {
+            if ("l".equals(str_list[i])) {
+                flag = 1;
+                idx = i;
                 break;
-            }
-            else if("r".equals(str_list[i])){
-                flag=-1;
-                idx=i;
+            } else if ("r".equals(str_list[i])) {
+                flag = -1;
+                idx = i;
                 break;
             }
         }
-        
-        if (flag == 1 && idx > 0) {
-            return Arrays.copyOfRange(str_list, 0, idx);
-        } else if (flag == -1 && idx < str_list.length - 1) {
-            return Arrays.copyOfRange(str_list, idx + 1, str_list.length);
-        } else {
-            return new String[0];
+
+        if (flag == 1) {
+            answer = new String[idx]; 
+            for (int i = 0; i < idx; i++) {
+                answer[i] = str_list[i];  
+            }
+        } else if (flag == -1) {
+            answer = new String[str_list.length - idx - 1]; 
+            for (int i = idx+1; i < str_list.length ; i++) {
+                answer[i - idx - 1] = str_list[i];
+            }
         }
-    }
-}
+        else
+            answer = new String[0];
+            
+
+        return answer;
+    }}
