9bc30f3f3be1e1946c93643836d6a93e143474f8
Jeongmin An <ajm010402@gmail.com>
2024-10-19 22:04:43 +0900

    [level 0] Title: 글자 지우기, Time: 22.55 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        int cnt=0;
+        Arrays.sort(indices);
+        
+        for(int i=0; i<my_string.length(); i++){
+                if(cnt < indices.length && i == indices[cnt])
+                    cnt++;
+                else
+                    answer+=my_string.charAt(i);
+            
+        }
+        return answer;     
+    }
+  
+}
