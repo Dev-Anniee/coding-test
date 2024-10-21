4b28fb0ff1fefcb9522e48828b38bdaffcebc9c4
Jeongmin An <ajm010402@gmail.com>
2024-10-21 18:57:13 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.05 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(String my_string, String alp) {
-        my_string=my_string.toLowerCase();
         my_string=my_string.replace(alp,alp.toUpperCase());
         return my_string;
     }
