4b334d5e77f05cc953fd55bac5922367614a7ebb
Jeongmin An <ajm010402@gmail.com>
2024-10-21 12:56:13 +0900

    [level 0] Title: 1로 만들기, Time: 0.04 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -3,12 +3,8 @@ class Solution {
         int answer = 0;
         for(int i=0; i<num_list.length; i++){
             while(num_list[i]!=1){
-                if(num_list[i]%2==0){
-                    num_list[i]=num_list[i]/2;
-                    answer++;}
-                else{
-                    num_list[i]=(num_list[i]-1)/2;
-                    answer++;}
+                    num_list[i]/=2;
+                    answer++;
             }
         }
         return answer;
