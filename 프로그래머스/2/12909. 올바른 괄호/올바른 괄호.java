baabd717eda935d934a6f1f784c30c90f4e5f69e
Jeongmin An <ajm010402@gmail.com>
2026-07-07 07:15:01 +0900

    [level 2] Title: 올바른 괄호, Time: 10.35 ms, Memory: 57.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
