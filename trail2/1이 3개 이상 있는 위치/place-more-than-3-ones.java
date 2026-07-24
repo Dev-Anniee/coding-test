ac14a282dc354a941ac200fdbd88a523d1eeb0fd
Jeongmin An <ajm010402@gmail.com>
2026-07-24 14:59:37 +0900

    [Passed] 1이 3개 이상 있는 위치 (121ms, 10MB)

--- trail2/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ trail2/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -1,38 +1,42 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       int N = Integer.parseInt(br.readLine());
-       int[][] grid = new int[N][N];
+    public static void main(String[] args) throws IOException{ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int[][] arr=new int[num][num];
 
-       for(int i=0; i<N; i++){
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int j=0; j<N; j++){
-            grid[i][j] = Integer.parseInt(st.nextToken());
-        }
-       }
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};     
+        int answer=0;  
 
-       int[] dx = {-1,1,0,0};
-       int[] dy = {0,0,-1,1};
-
-       int answer=0;
-       for(int x=0; x<N; x++){
-        for(int y=0; y<N; y++){
-           int count =0;
-           for(int i=0; i<4; i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-
-            if(nx>=0&&nx<N&&ny>=0&&ny<N){
-                if(grid[nx][ny]==1)
-                    count++;
+        for(int i=0; i<num; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<num; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
-           }
-            if(count>=3)
-                answer++;
         }
-       }
-       System.out.println(answer);
+
+        for(int x=0; x<num; x++){
+            for(int y=0; y<num; y++){
+            int count=0;
+                for(int i=0; i<4; i++){
+                    int nx = x+dx[i];
+                    int ny = y+dy[i];
+
+                    if(0 <=nx && nx < num && 0 <= ny && ny < num){
+                        if(arr[nx][ny]==1)
+                            count++;
+                    }
+
+                    if (count == 3) {
+                        answer++;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
     }
 }
