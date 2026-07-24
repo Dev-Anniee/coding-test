64fea20a1122f4665d85365eaf11e48c6292188a
Jeongmin An <ajm010402@gmail.com>
2026-07-24 09:55:59 +0900

    [Passed] 방향에 맞춰 이동 (97ms, 9MB)

--- trail2/방향에 맞춰 이동/move-in-direction.java
+++ trail2/방향에 맞춰 이동/move-in-direction.java
@@ -1,31 +1,31 @@
 import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) throws IOException{
-        int dx=0, dy =0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
 
-        for (int i =0; i<num; i++){
-            String line = br.readLine();
+        // 0: N, 1: E, 2: S, 3: W 순서
+        int[] dx = {0,1,0,-1};
+        int[] dy = {1,0,-1,0};
+        int nx=0, ny=0;
 
-            char dir = line.charAt(0);
-            int dist = Integer.parseInt(line.substring(2));
-
-            switch (dir) {
-                case 'N':
-                    dy += dist;
-                    break;
-                case 'S':
-                    dy -= dist;
-                    break;
-                case 'E':
-                    dx += dist;
-                    break;
-                case 'W':
-                    dx -= dist;
-                    break;
-            }
+        for(int i=0; i<num; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char dir = st.nextToken().charAt(0);
+            int move = Integer.parseInt(st.nextToken());
+            
+            int dirNum = 0;
+            if (dir == 'N') dirNum = 0;
+            else if (dir == 'E') dirNum = 1;
+            else if (dir == 'S') dirNum = 2;
+            else if (dir == 'W') dirNum = 3;
+            
+            nx += dx[dirNum] * move;
+            ny += dy[dirNum] * move;
         }
-        System.out.println(dx+" "+dy);
+        
+        System.out.println(nx +" "+ny);
     }
 }
