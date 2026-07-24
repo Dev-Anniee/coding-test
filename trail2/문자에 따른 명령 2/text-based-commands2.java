3061f3fc69f698db81384e8bb7f67cf110373f43
Jeongmin An <ajm010402@gmail.com>
2026-07-24 11:16:46 +0900

    [Passed] 문자에 따른 명령 2 (94ms, 9MB)

--- trail2/문자에 따른 명령 2/text-based-commands2.java
+++ trail2/문자에 따른 명령 2/text-based-commands2.java
@@ -1,28 +1,30 @@
+import java.util.*;
 import java.io.*;
-
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
-        
-        //북(0), 동(1), 남(2), 서(3) 
-        int[] dx = new int[] {0,1,0,-1};
-        int[] dy = new int[] {1,0,-1,0};
+
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
 
         int x=0, y=0;
-        int dir =0; //초기방향 북
+        int dir =0; 
 
-        for(int i =0; i<line.length(); i++){
-            char ch = line.charAt(i);
-            if(ch == 'R')
-                dir = (dir+1)%4;
-            else if (ch=='L')
-                dir = (dir+3)%4;
-            else if(ch=='F'){
-                x+=dx[dir];
-                y+=dy[dir];
+        for(char command : line.toCharArray()){
+            switch(command){
+                case 'R':
+                    dir = (dir+1)%4;
+                    break;
+                case 'L':
+                    dir = (dir+3)%4;
+                    break;
+                case 'F':
+                    x+=dx[dir];             
+                    y+=dy[dir];
+                    break;
             }
         }
-        System.out.println(x+" "+y);
-    }   
+        System.out.println(x + " " + y );
+    }
 }
