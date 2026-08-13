30d07471c45881d55fb3efa4da1869278b21863e
Jeongmin An <ajm010402@gmail.com>
2026-08-13 14:32:48 +0900

    [Passed] 2진수로 변환하기 (128ms, 11MB)

---
+++ trail2/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        int[] digits = new int[32];
+        int cnt = 0;
+
+        while (n > 0) {
+            digits[cnt++] = n % 2;
+            n /= 2;
+        }
+
+        for (int i = cnt - 1; i >= 0; i--) {
+            System.out.print(digits[i]);
+        }
+        
+        System.out.println(); 
+    }
+}
