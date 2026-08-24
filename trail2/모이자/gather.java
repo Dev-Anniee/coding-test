da0d1b0196115c63eb218359f2d188a086eee4db
Jeongmin An <ajm010402@gmail.com>
2026-08-24 22:30:15 +0900

    [Passed] 모이자 (143ms, 10MB)

---
+++ trail2/모이자/gather.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int answer = INT_MAX;
+        for(int i=0; i<n; i++){
+            int sum=0;
+            for(int j=0; j<n; j++){
+                sum+=a[j]*Math.abs(i-j);
+            }
+            if(sum<answer)
+                answer=sum;
+        }
+        System.out.print(answer);
+    }
+}
