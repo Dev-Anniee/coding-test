e70ec35c8986264a01a2fe728929cc89491de78d
Jeongmin An <ajm010402@gmail.com>
2026-09-10 22:01:21 +0900

    [Algo Room] 피보나치 수 (Java)

--- algoroom/algorithm/pg-12945/java/Solution.java
+++ algoroom/algorithm/pg-12945/java/Solution.java
@@ -1,5 +1,8 @@
 class Solution {
     public int solution(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
         int f0 = 0, f1 =1;
         int answer = 0;
         for(int i=1; i<n; i++){
