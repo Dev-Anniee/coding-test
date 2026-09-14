1aeb1999e73952bfa261881e239de890771ca17d
Jeongmin An <ajm010402@gmail.com>
2026-09-14 09:35:12 +0900

    [Algo Room] 타겟 넘버 (Java)

---
+++ algoroom/algorithm/pg-43165/java/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+    private void dfs (int[] numbers, int target, int dept, int sum){
+        if(dept == numbers.length){
+            if(sum==target)
+                answer++;
+            return; 
+        }
+        dfs(numbers, target, dept+1, sum+numbers[dept]);
+        dfs(numbers, target, dept+1, sum-numbers[dept]);
+    }
+}
